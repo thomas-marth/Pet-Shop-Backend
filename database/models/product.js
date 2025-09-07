9b7bf140a63ee56f866f6a07604445bd8554598a
Dmytro Bazhenov <63171863+thomas-marth@users.noreply.github.com>
2025-09-07 16:46:39 +0200

    Update product.js

--- database/models/product.js
+++ database/models/product.js
@@ -1,18 +1,17 @@
-const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
+const { DataTypes } = require('sequelize');
 
 const Product = sequelize.define('Product', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  title: { type: DataTypes.TEXT, allowNull: false },
-  price: { type: DataTypes.INTEGER, allowNull: false },
-  discont_price: { type: DataTypes.INTEGER, allowNull: true },
-  description: { type: DataTypes.TEXT, allowNull: true },
-  image: { type: DataTypes.TEXT, allowNull: true },
-
-  categoryId: { type: DataTypes.INTEGER, allowNull: true }
-}, {
-  tableName: 'products',
-  timestamps: false
-});
+  title: DataTypes.TEXT,
+  price: DataTypes.INTEGER,
+  discont_price: DataTypes.INTEGER,
+  description: DataTypes.TEXT,
+  image: DataTypes.TEXT,
+  categoryId: DataTypes.INTEGER,
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE
+}, { tableName: 'products', timestamps: true });
 
 module.exports = Product;
+

1d8fd3c0b0b019e3259797e551581ac8d8ba0d8a
Dmytro Bazhenov <63171863+thomas-marth@users.noreply.github.com>
2025-09-07 17:28:19 +0200

    Update product.js

--- database/models/product.js
+++ database/models/product.js
@@ -1,17 +1,28 @@
-const sequelize = require('../database');
+const sequelize = require('../database'); 
 const { DataTypes } = require('sequelize');
 
 const Product = sequelize.define('Product', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  title: DataTypes.TEXT,
-  price: DataTypes.INTEGER,
+  id:            { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  title:         DataTypes.TEXT,
+  price:         DataTypes.INTEGER,
   discont_price: DataTypes.INTEGER,
-  description: DataTypes.TEXT,
-  image: DataTypes.TEXT,
-  categoryId: DataTypes.INTEGER,
-  createdAt: DataTypes.DATE,
-  updatedAt: DataTypes.DATE
-}, { tableName: 'products', timestamps: true });
+  description:   DataTypes.TEXT,
+  image:         DataTypes.TEXT,
+  categoryId:    DataTypes.INTEGER,
+  createdAt:     DataTypes.DATE,
+  updatedAt:     DataTypes.DATE
+}, {
+  tableName: 'products',
+  timestamps: true,       
+  defaultScope: {
+    order: [['id', 'ASC']]  
+  },
+  // (опционально) полезные именованные сортировки
+  scopes: {
+    newest:     { order: [['createdAt', 'DESC'], ['id', 'DESC']] },
+    cheapFirst: { order: [['price', 'ASC'],      ['id', 'ASC']]  },
+    priceyFirst:{ order: [['price', 'DESC'],     ['id', 'ASC']]  }
+  }
+});
 
 module.exports = Product;
-
